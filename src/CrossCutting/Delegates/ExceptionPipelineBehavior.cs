dd21d285b2424a6c55a06ac2779b0c55961f9263
guisatech <guisatech@hotmail.com>
2025-11-02 20:41:41 -0300

    :sparkles: feat: adding crosscutting layer

---
+++ src/CrossCutting/Delegates/ExceptionPipelineBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Shared.Results.Errors.Default;
+
+namespace CrossCutting.Delegates;
+
+public class ExceptionPipelineBehavior<TRequest, TResponse>(
+    ILogger<ExceptionPipelineBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse> where TResponse : ResultBase, new()
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An unexpected error occurred");
+
+            var response = new TResponse();
+            response.Reasons.Add(new InternalServerError("An unexpected error occurred, please try again later."));
+
+            return response;
+        }
+    }
+}
---
+++ src/CrossCutting/Delegates/ValidatorPipelineBehavior.cs
@@ -0,0 +1,41 @@
+﻿using FluentResults;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace CrossCutting.Delegates;
+
+
+public class ValidatorPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : ResultBase, new()
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (!validators.Any()) return await next();
+
+        var errorList = validators
+            .Select(validator => validator.Validate(request))
+            .SelectMany(validatorResult => validatorResult.Errors)
+            .Where(validatorFailure => validatorFailure is not null)
+            .ToList();
+
+        if (errorList.Count <= 0)
+            return await next();
+
+        var result = BuildResponse<TResponse>(errorList);
+        return result;
+    }
+
+    private static TResult BuildResponse<TResult>(IEnumerable<ValidationFailure> errors)
+        where TResult : TResponse, new()
+    {
+        var result = new TResult();
+        var errorList = errors.Select(e => new Error(e.ErrorMessage));
+        result.Reasons.AddRange(errorList);
+
+        return result;
+    }
+}
